3703dceab8f631db7cb540c0a89b4153ca798670
Andrew McPherson <andrew.mcpherson@gmail.com>
2014-05-19 11:33:00 -0700

    download and index genome

--- info.py
+++ info.py
@@ -2,17 +2,23 @@ import os
 
 tutorial_directory = '/Users/amcphers/Analysis/cbw_tutorial'
 
-ensembl_gtf_url = 'ftp://ftp.ensembl.org/pub/release-75/gtf/homo_sapiens/Homo_sapiens.GRCh37.75.gtf.gz'
-ensembl_chr20_url = 'ftp://ftp.ensembl.org/pub/release-75/fasta/homo_sapiens/dna/Homo_sapiens.GRCh37.75.dna.chromosome.20.fa.gz'
-
 data_directory = os.path.join(tutorial_directory, 'data')
 genome_directory = os.path.join(tutorial_directory, 'genome')
 install_directory = os.path.join(tutorial_directory, 'install')
 results_directory = os.path.join(tutorial_directory, 'results')
 
+ensembl_gtf_url = 'ftp://ftp.ensembl.org/pub/release-75/gtf/homo_sapiens/Homo_sapiens.GRCh37.75.gtf.gz'
+ensembl_chr20_url = 'ftp://ftp.ensembl.org/pub/release-75/fasta/homo_sapiens/dna/Homo_sapiens.GRCh37.75.dna.chromosome.20.fa.gz'
+
 ensembl_gtf_filename = os.path.join(genome_directory, 'Homo_sapiens.GRCh37.75.gtf.gz')
 ensembl_chr20_filename = os.path.join(genome_directory, 'Homo_sapiens.GRCh37.75.dna.chromosome.20.fa.gz')
 
+hg19_url = 'ftp://hgdownload.cse.ucsc.edu/goldenPath/hg19/bigZips/chromFa.tar.gz'
+hg19_tar_filename = os.path.join(genome_directory, 'chromFa.tar.gz')
+hg19_filename = os.path.join(genome_directory, 'hg19.fa')
+hg19_index_base = os.path.join(genome_directory, 'hg19')
+
+chromosomes = [str(a) for a in xrange(1, 23)] + ['X', 'Y', 'MT']
 
 sra_samples = ['SRR064439', 'SRR201779']
 
--- prepare_data.py
+++ prepare_data.py
@@ -1,6 +1,8 @@
 import os
 import subprocess
 import sys
+import tarfile
+import shutil
 
 import utils
 import info
@@ -11,6 +13,28 @@ sentinal = utils.AutoSentinal(os.path.join(info.data_directory, 'sentinal_'))
 
 sentinal.run('download_ensembl_gtf', utils.wget_file, info.ensembl_gtf_url, info.ensembl_gtf_filename)
 sentinal.run('download_ensembl_chr20', utils.wget_file, info.ensembl_chr20_url, info.ensembl_chr20_filename)
+sentinal.run('download_hg19', utils.wget_file, info.hg19_url, info.hg19_tar_filename)
+
+
+def create_genome():
+
+    with open(info.hg19_filename, 'w') as hg19_file, tarfile.open(info.hg19_tar_filename, 'r:gz') as tar:
+
+        for tarinfo in tar:
+
+            chromosome = tarinfo.name[3:-3]
+
+            if chromosome in info.chromosomes:
+                shutil.copyfileobj(tar.extractfile(tarinfo), hg19_file)
+
+sentinal.run('extract_hg19', create_genome)
+
+
+def index_genome():
+
+    subprocess.check_call(['bowtie2-build', info.hg19_filename, info.hg19_index_base])
+
+sentinal.run('index_hg19', index_genome)
 
 
 for sra_id in info.sra_samples:
