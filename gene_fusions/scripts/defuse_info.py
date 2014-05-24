96a47414ea0d35a3418b7074bae188531facfb91
Andrew McPherson <andrew.mcpherson@gmail.com>
2014-05-23 18:36:35 -0700

    defuse

--- gene_fusions/scripts/defuse_info.py
+++ gene_fusions/scripts/defuse_info.py
@@ -8,3 +8,5 @@ install_directory = os.path.join(info.install_directory, 'defuse')
 
 packages_directory = os.path.join(install_directory, 'packages')
 bin_directory = os.path.join(install_directory, 'bin')
+data_directory = os.path.join(install_directory, 'data')
+
--- gene_fusions/scripts/defuse_install.py
+++ gene_fusions/scripts/defuse_install.py
@@ -34,3 +34,23 @@ with Sentinal('install') as sentinal:
 
                 subprocess.check_call(['make'])
 
+            config_filename = os.path.join(defuse_info.data_directory, 'config.txt')
+            template_config_filename = os.path.join(extract_dir, 'scripts', 'config.txt')
+
+            with open(config_filename, 'r') as config_file, open(template_config_filename, 'r') as template_config_file:
+
+                for line in template_config_file:
+                    if not line.startswith('#') and '=' in line:
+                        key, value = line.split('=')
+                        key = key.strip()
+                        value = value.strip()
+
+                        if key == 'source_directory':
+                            line = 'source_director = {0}\n'.format(extract_dir)
+                        elif key == 'dataset_directory':
+                            line = 'dataset_directory = {0}\n'.format(defuse_info.data_directory)
+                        elif '[path of your' in value:
+                            line = '{0} = {1}\n'.format(key, value.split(' ')[3])
+                    
+                    config_file.write(line)
+                    
