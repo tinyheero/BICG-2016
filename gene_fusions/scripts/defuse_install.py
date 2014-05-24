59c33a76c51f24772284a011711606407f0d8401
Andrew McPherson <andrew.mcpherson@gmail.com>
2014-05-23 18:40:21 -0700

    bugfix

--- gene_fusions/scripts/defuse_install.py
+++ gene_fusions/scripts/defuse_install.py
@@ -22,6 +22,8 @@ with Sentinal('install') as sentinal:
         utils.rmtree(defuse_info.bin_directory)
         utils.makedirs(defuse_info.bin_directory)
 
+        utils.makedirs(defuse_info.data_directory)
+
         with utils.CurrentDirectory(defuse_info.packages_directory):
 
             subprocess.check_call('wget --no-check-certificate http://downloads.sourceforge.net/project/defuse/defuse/0.6/defuse-0.6.1.tar.gz'.split(' '))
@@ -37,7 +39,7 @@ with Sentinal('install') as sentinal:
             config_filename = os.path.join(defuse_info.data_directory, 'config.txt')
             template_config_filename = os.path.join(extract_dir, 'scripts', 'config.txt')
 
-            with open(config_filename, 'r') as config_file, open(template_config_filename, 'r') as template_config_file:
+            with open(config_filename, 'w') as config_file, open(template_config_filename, 'r') as template_config_file:
 
                 for line in template_config_file:
                     if not line.startswith('#') and '=' in line:
