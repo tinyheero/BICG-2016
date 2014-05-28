df5105ec32f579a7ec3b4b0cc1b400ccd5e87642
Andrew McPherson <amcphers@bccrc.ca>
2014-05-27 22:32:49 -0400

    oncosnpseq run

---
+++ copy_number/scripts/run_oncosnpseq.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+TUMOUR_FILE=$1
+
+NORMAL_FILE=$2
+
+SAMPLE_NAME=$3
+
+OUTPUT_DIR=$4
+
+#### OncoSNP Parameter Files ####
+
+STATES_FILE=$ONCOSNPSEQ_DIR/config/tumourstates.txt
+
+HGTABLES_FILE=$ONCOSNPSEQ_DIR/config/hgTables_b37.txt
+
+############## EXECUTE ONCOSNP ####################################################################################
+############## For more information, see https://sites.google.com/site/oncosnpseq/usage                 ###########
+###################################################################################################################
+
+$ONCOSNP_DIR/run_oncosnp.sh $MCR_DIR \
+    --tumourstatesfile $STATES_FILE \
+    --hgtables $HGTABLES_FILE \
+    --infile $TUMOUR_FILE \
+    --normalfile $NORMAL_FILE \
+    --samplename $SAMPLE_NAME \
+    --seqtype 'illumina' \
+    --outdir $OUTPUT_DIR
+    >$OUTPUT_DIR/run.log 2>$OUTPUT_DIR/run.err
+
+
+
+
+
